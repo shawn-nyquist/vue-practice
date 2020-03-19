14bf64023076f0f2ecf981c5d944ea1f89dc3105
shawn-nyquist <shawn.nyquist@gmail.com>
2020-03-19 15:23:04 -0600

    Display question box with answers and cycle questions

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
     <Header />
+
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
-          <QuestionBox />
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -21,6 +26,28 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  methods: {
+    next: function() {
+      this.index++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=26&type=multiple', {
+      method: 'get'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
+      })
   }
 };
 </script>
--- vue-practice/src/components/QuestionBox.vue
+++ vue-practice/src/components/QuestionBox.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
     <b-jumbotron>
-      <template v-slot:lead>
-        Some Questions...
-      </template>
+      <template v-slot:lead>{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
 
-      <p>
-        Answers go here...
+      <p v-for="(answer, index) in answers" :key="index">
+          {{answer}}
       </p>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next Question</b-button>
+      <b-button @click="next" variant="success" href="#">Next Question</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+      answers() {
+         let answers = [...this.currentQuestion.incorrect_answers]
+         answers.push(this.currentQuestion.correct_answer)
+         return answers
+      }
+  }
+};
+</script>
