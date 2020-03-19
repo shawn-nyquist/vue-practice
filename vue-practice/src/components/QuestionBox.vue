5665b05e522c16bf541066a05de5393f806062ed
shawn-nyquist <shawn.nyquist@gmail.com>
2020-03-19 14:48:39 -0600

    Created Project and created basic question box

---
+++ vue-practice/src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>
+        Some Questions...
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        Answers go here...
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next Question</b-button>
+    </b-jumbotron>
+  </div>
+</template>
